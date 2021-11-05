ed44b74972a62f1ab67fc7e3f7f91976cbf2a1e4
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2021-11-05 18:31:31 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTAC0GZJ4KvwOhnnSg2KA-1OpvzwO2BHU",
+    authDomain: "smile-1a9ba.firebaseapp.com",
+    projectId: "smile-1a9ba",
+    storageBucket: "smile-1a9ba.appspot.com",
+    messagingSenderId: "358221204636",
+    appId: "1:358221204636:web:8bf46ebb96a61b5f29b159"
+  };
