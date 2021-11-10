0dce3030d411674ff72e725c4dee7c2a0da52eea
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2021-11-10 18:27:26 -0500

    initital commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <div className="home__container">
+                    <img className="home__image" src="https://i.imgur.com/SUeDrDa.png" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
