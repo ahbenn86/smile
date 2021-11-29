d233b095d493a360dc91e148c7a4652d828fcf86
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2021-11-28 20:55:57 -0500

    created product checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import "./Checkout.css";
 import Subtotal from './Subtotal';
+import {useStateValue} from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
+
 
 
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
-              <img className="checkout__ad" src="https://i.imgur.com/7QOnC20.png" alt=""/> 
+              <img className="checkout__ad" src="https://i.imgur.com/0m5n5tt.png" alt=""/> 
               {/* size is 1920x93 */}
               <div>
               <h2 className="checkout__title">
                     Your Smile Cart
                 </h2>
+                {basket.map(item => (
+                    <CheckoutProduct
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    rating={item.rating}
+                    />
+                ))}
               </div>
             </div>
             <div className="checkout__right">
