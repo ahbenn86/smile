7d3f66c69cf707f57daae1caac54a7a71e989adb
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2021-12-11 22:17:38 -0500

    added auth state change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import { useStateValue } from './StateProvider';
 
+const auth = getAuth()
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      console.log('THE USER IS >>>', user);
+
+      if (user){
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   let location = useLocation();
   return (
     <div>
